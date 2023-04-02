3d63b0696a51b2e7bd95aa74fa9f018854d87f1f
Enak1n <masj963@gmail.com>
2023-04-02 19:52:11 +0300

    WorkingRelease

---
+++ CalculatorWPF/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+namespace LoginPage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (UIElement item in MainGrid.Children)
+            {
+                if (item is Button)
+                    ((Button)item).Click += ButtonClick;
+            }
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+
+            if (str == "AC")
+            {
+                textLabel.Text = "";
+            }
+            else if (str == "=")
+            {
+                string? value = new DataTable().Compute(textLabel.Text, null).ToString();
+                textLabel.Text = value;
+            }
+            else
+                textLabel.Text += str;
+        }
+    }
+}
